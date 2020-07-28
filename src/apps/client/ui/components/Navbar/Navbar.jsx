985c611a06e60ce223e3315311fe042c11d8b3ad
pseleznov <pashaselesniov1@gmail.com>
2020-07-29 01:03:25 +0300

    add ua navbar

--- src/apps/client/ui/components/Navbar/Navbar.jsx
+++ src/apps/client/ui/components/Navbar/Navbar.jsx
@@ -4,10 +4,73 @@ import NavbarItem from '../NavbarItem/NavbarItem';
 
 import styles from './Navbar.module.css'
 
+const NAVBAR = {
+    ua: [
+        {
+            title: 'Чоловічі сорочки',
+            subtitle: []
+        },
+        {
+            title: 'Жіночі блузки',
+            subtitle: []
+        },
+        {
+            title: 'Жіночі сукні',
+            subtitle: []
+        },
+        {
+            title: 'Жіночі сукні',
+            subtitle: []
+        },
+        {
+            title: 'Парні костюми',
+            subtitle: []
+        },
+        {
+            title: 'Для дівчаток',
+            subtitle: [
+                'Блузки',
+                'Плаття',
+                'Спіднички'
+            ]
+        },
+        {
+            title: 'Для хлопчиків',
+            subtitle: [
+                'Сорочки',
+                'Шаровари'
+            ]
+        },
+        {
+            title: 'Сценічний одяг',
+            subtitle: []
+        },
+        {
+            title: 'Спортивні костюми',
+            subtitle: []
+        },
+        {
+            title: 'Оформлення сцен',
+            subtitle: []
+        },
+        {
+            title: 'Дрібнички і не тільки',
+            subtitle: []
+        }
+    ]
+};
+
 const Navbar = () => {
     return <div className={styles.navbarContainer}>
-        <NavbarItem title='Вишиванки' subTitles={['Такі', 'Сякі']} />
-        <NavbarItem title='Шортики' subTitles={['маленькі', 'великі']} />
+        {NAVBAR.ua.map((item, i) => {
+            return (
+                <NavbarItem
+                    title={item.title}
+                    subTitles={item.subtitle}
+                    key={i}
+                />
+            );
+        })}
     </div>
 }
 
