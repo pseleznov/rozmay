e84d6b1d0deb1fd01363d5bf0b1c2e3b0ba7ff7e
pseleznov <pashaselesniov1@gmail.com>
2020-07-30 00:21:29 +0300

    navbar refactoring

--- src/apps/client/ui/components/Navbar/Navbar.jsx
+++ src/apps/client/ui/components/Navbar/Navbar.jsx
@@ -1,77 +1,138 @@
-import React from 'react'
+import React, { useState } from 'react';
+import classNames from 'classnames';
 
-import NavbarItem from '../NavbarItem/NavbarItem';
+import { NavLink } from 'react-router-dom';
 
 import styles from './Navbar.module.css'
 
-const NAVBAR = {
-    ua: [
-        {
-            title: 'Чоловічі сорочки',
-            subtitle: []
-        },
-        {
-            title: 'Жіночі блузки',
-            subtitle: []
-        },
-        {
-            title: 'Жіночі сукні',
-            subtitle: []
-        },
-        {
-            title: 'Жіночі сукні',
-            subtitle: []
-        },
-        {
-            title: 'Парні костюми',
-            subtitle: []
-        },
-        {
-            title: 'Для дівчаток',
-            subtitle: [
-                'Блузки',
-                'Плаття',
-                'Спіднички'
-            ]
-        },
-        {
-            title: 'Для хлопчиків',
-            subtitle: [
-                'Сорочки',
-                'Шаровари'
-            ]
-        },
-        {
-            title: 'Сценічний одяг',
-            subtitle: []
-        },
-        {
-            title: 'Спортивні костюми',
-            subtitle: []
-        },
-        {
-            title: 'Оформлення сцен',
-            subtitle: []
-        },
-        {
-            title: 'Дрібнички і не тільки',
-            subtitle: []
-        }
+const subtitles = {
+    girls: [
+        { title: 'Блузки', link: '/' },
+        { title: 'Плаття', link: '/' },
+        { title: 'Спіднички', link: '/' }
+    ],
+    boys: [
+        { title: 'Сорочки', link: '/' },
+        { title: 'Шаровари', link: '/' },
     ]
 };
 
 const Navbar = () => {
-    return <div className={styles.navbarContainer}>
-        {NAVBAR.ua.map((item, i) => {
-            return (
-                <NavbarItem
-                    title={item.title}
-                    subTitles={item.subtitle}
-                    key={i}
-                />
-            );
-        })}
-    </div>
-}
+    const [showGirlsSubItems, handleGirlsClick] = useState(false);
+    const [showBoysSubItems, handleBoysClick] = useState(false);
+
+    return (
+        <div className={styles.navbarContainer}>
+            <div className={styles.navbarItem}>
+                <NavLink
+                    exact to={'/'}
+                    activeClassName={styles.active}
+                >
+                    Чоловічі сорочки
+                </NavLink>
+            </div>
+            <div className={styles.navbarItem}>
+                <NavLink
+                    exact to={'/'}
+                    activeClassName={styles.active}
+                >
+                    Жіночі блузки
+                </NavLink>
+            </div>
+            <div className={styles.navbarItem}>
+                <NavLink
+                    exact to={'/'}
+                    activeClassName={styles.active}
+                >
+                    Жіночі сукні
+                </NavLink>
+            </div>
+            <div className={styles.navbarItem}>
+                <NavLink
+                    exact to={'/'}
+                    activeClassName={styles.active}
+                >
+                    Парні костюми
+                </NavLink>
+            </div>
+            <div className={classNames(styles.navbarItem, styles.group)} onClick={() => handleGirlsClick(!showGirlsSubItems)}>
+                Одяг для дівчаток
+                <div className={classNames(styles.itemContent, {
+                [styles.disable]: !showGirlsSubItems
+            })}>
+                    {subtitles.girls.map((item, i) => {
+                        return (
+                            <div
+                                className={styles.subItem}
+                                key={i}
+                            >
+                                <NavLink
+                                    exact to={'/'}
+                                    activeClassName={styles.active}
+                                >
+                                    {item.title}
+                                </NavLink>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+            <div className={classNames(styles.navbarItem, styles.group)} onClick={() => handleBoysClick(!showBoysSubItems)}>
+                Одяг для хлопчиків
+                <div className={classNames(styles.itemContent, {
+                [styles.disable]: !showBoysSubItems
+            })}>
+                    {subtitles.boys.map((item, i) => {
+                        return (
+                            <div
+                                className={styles.subItem}
+                                key={i}
+                            >
+                                <NavLink
+                                    exact to={'/'}
+                                    activeClassName={styles.active}
+                                >
+                                    {item.title}
+                                </NavLink>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+            <div className={styles.navbarItem}>
+                <NavLink
+                    exact to={'/'}
+                    activeClassName={styles.active}
+                >
+                    Сценічний одяг
+                </NavLink>
+            </div>
+            <div className={styles.navbarItem}>
+                <NavLink
+                    exact to={'/'}
+                    activeClassName={styles.active}
+                >
+                    Спортивні костюми
+                </NavLink>
+            </div>
+            <div className={styles.navbarItem}>
+                <NavLink
+                    exact to={'/'}
+                    activeClassName={styles.active}
+                >
+                    Оформлення сцен
+                </NavLink>
+            </div>
+            <div className={styles.navbarItem}>
+                <NavLink
+                    exact to={'/'}
+                    activeClassName={styles.active}
+                >
+                    Дрібнички і не тільки
+                </NavLink>
+            </div>
+        </div>
+    );
+};
 
 export default Navbar;
--- src/seeds/data.js
+++ src/seeds/data.js
@@ -50,4 +50,112 @@ const data = [
     ]
 ];
 
+const data2 = [
+    {
+        productId: 'mens-shirt',
+        value: [
+            {
+
+            }
+        ]
+    },
+    {
+        productId: 'womens-blouses',
+        value: [
+            {
+
+            }
+        ]
+    },
+    {
+        productId: 'womens-dresses',
+        value: [
+            {
+
+            }
+        ]
+    },
+    {
+        productId: 'paired-suits',
+        value: [
+            {
+
+            }
+        ]
+    },
+    {
+        productId: 'clothes-for-girls-blouses',
+        value: [
+            {
+
+            }
+        ]
+    },
+    {
+        productId: 'clothes-for-girls-dresses',
+        value: [
+            {
+
+            }
+        ]
+    },
+    {
+        productId: 'clothes-for-girls-skirts',
+        value: [
+            {
+
+            }
+        ]
+    },
+    {
+        productId: 'clothes-for-boys-shirt',
+        value: [
+            {
+
+            }
+        ]
+    },
+    {
+        productId: 'clothes-for-boys-trousers',
+        value: [
+            {
+
+            }
+        ]
+    },
+    {
+        productId: 'stage-clothes',
+        value: [
+            {
+
+            }
+        ]
+    },
+    {
+        productId: 'tracksuits',
+        value: [
+            {
+
+            }
+        ]
+    },
+    {
+        productId: 'scene-design',
+        value: [
+            {
+
+            }
+        ]
+    },
+    {
+        productId: 'trifles',
+        value: [
+            {
+
+            }
+        ]
+    }
+]
+
+
 export default data;
