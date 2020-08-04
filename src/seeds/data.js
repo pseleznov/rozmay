d28f71bfaf75ce8ef0760325a0449b0ae6eff9ce
pseleznov <pashaselesniov1@gmail.com>
2020-08-04 23:19:21 +0300

    create product objects

--- src/seeds/data.js
+++ src/seeds/data.js
@@ -3,20 +3,89 @@ const data = [
         productId: 'men-shirt',
         value: [
             {
+                id: 1,
                 price: 'від 100 грн',
                 photo: 'https://i.imgur.com/A7PTQVH.jpg'
             },
             {
+                id: 2,
                 price: 'від 100 грн',
-                photo: 'https://i.imgur.com/A7PTQVH.jpg'
+                photo: 'https://i.imgur.com/zXoxniz.jpg'
             },
             {
+                id: 3,
                 price: 'від 100 грн',
-                photo: 'https://i.imgur.com/A7PTQVH.jpg'
+                photo: 'https://i.imgur.com/jXuJl3B.jpg'
             },
             {
+                id: 4,
                 price: 'від 100 грн',
-                photo: 'https://i.imgur.com/A7PTQVH.jpg'
+                photo: 'https://i.imgur.com/NP0NtTc.jpg'
+            },
+            {
+                id: 5,
+                price: 'від 100 грн',
+                photo: 'https://i.imgur.com/MGWu6GG.jpg'
+            },
+            {
+                id: 6,
+                price: 'від 100 грн',
+                photo: 'https://i.imgur.com/ZXrB4Rz.jpg'
+            },
+            {
+                id: 7,
+                price: 'від 100 грн',
+                photo: 'https://i.imgur.com/yiMHoBI.jpg'
+            },
+            {
+                id: 8,
+                price: 'від 100 грн',
+                photo: 'https://i.imgur.com/RSzMEi9.jpg'
+            },
+            {
+                id: 9,
+                price: 'від 100 грн',
+                photo: 'https://i.imgur.com/TKUNBZj.jpg'
+            },
+            {
+                id: 10,
+                price: 'від 100 грн',
+                photo: 'https://i.imgur.com/wG10X0B.jpg'
+            },
+            {
+                id: 11,
+                price: 'від 100 грн',
+                photo: 'https://i.imgur.com/CWykPPI.jpg'
+            },
+            {
+                id: 12,
+                price: 'від 100 грн',
+                photo: 'https://i.imgur.com/MpuGs4q.jpg'
+            },
+            {
+                id: 13,
+                price: 'від 100 грн',
+                photo: 'https://i.imgur.com/sazq8ZP.jpg'
+            },
+            {
+                id: 14,
+                price: 'від 100 грн',
+                photo: 'https://i.imgur.com/oiplPgG.jpg'
+            },
+            {
+                id: 15,
+                price: 'від 100 грн',
+                photo: 'https://i.imgur.com/wuBAtJ4.jpg'
+            },
+            {
+                id: 16,
+                price: 'від 100 грн',
+                photo: 'https://i.imgur.com/LLe824b.jpg'
+            },
+            {
+                id: 17,
+                price: 'від 100 грн',
+                photo: 'https://i.imgur.com/ZRLUJjL.jpg'
             }
         ]
     },
@@ -24,34 +93,336 @@ const data = [
         productId: 'women-blouses',
         value: [
             {
+                id: 1,
                 price: 'від 200 грн',
                 photo: 'https://i.imgur.com/Wy2i7V4.jpg'
             },
             {
+                id: 2,
                 price: 'від 200 грн',
-                photo: 'https://i.imgur.com/Wy2i7V4.jpg'
+                photo: 'https://i.imgur.com/JxflcVh.jpg'
             },
             {
+                id: 3,
                 price: 'від 200 грн',
-                photo: 'https://i.imgur.com/Wy2i7V4.jpg'
+                photo: 'https://i.imgur.com/jTb8P2g.jpg'
+            },
+            {
+                id: 4,
+                price: 'від 200 грн',
+                photo: 'https://i.imgur.com/hSrXYzM.jpg'
+            },
+            {
+                id: 5,
+                price: 'від 200 грн',
+                photo: 'https://i.imgur.com/XPGk2Re.jpg'
+            },
+            {
+                id: 6,
+                price: 'від 200 грн',
+                photo: 'https://i.imgur.com/UTxZSCA.jpg'
+            },
+            {
+                id: 7,
+                price: 'від 200 грн',
+                photo: 'https://i.imgur.com/NdWnCLg.jpg'
+            },
+            {
+                id: 8,
+                price: 'від 200 грн',
+                photo: 'https://i.imgur.com/Tc4akQG.jpg'
+            },
+            {
+                id: 9,
+                price: 'від 200 грн',
+                photo: 'https://i.imgur.com/iCl1aDF.jpg'
+            },
+            {
+                id: 10,
+                price: 'від 200 грн',
+                photo: 'https://i.imgur.com/qKP6jFY.jpg'
+            },
+            {
+                id: 11,
+                price: 'від 200 грн',
+                photo: 'https://i.imgur.com/DW1ONaW.jpg'
+            },
+            {
+                id: 12,
+                price: 'від 200 грн',
+                photo: 'https://i.imgur.com/vFDDRBu.jpg'
+            },
+            {
+                id: 13,
+                price: 'від 200 грн',
+                photo: 'https://i.imgur.com/SDeES7v.jpg'
+            },
+            {
+                id: 14,
+                price: 'від 200 грн',
+                photo: 'https://i.imgur.com/FWCOghH.jpg'
+            },
+            {
+                id: 15,
+                price: 'від 200 грн',
+                photo: 'https://i.imgur.com/Yc8UTdo.jpg'
+            },
+            {
+                id: 16,
+                price: 'від 200 грн',
+                photo: 'https://i.imgur.com/8HiOh7w.jpg'
+            },
+            {
+                id: 17,
+                price: 'від 200 грн',
+                photo: 'https://i.imgur.com/4Hp1ogd.jpg'
+            },
+            {
+                id: 18,
+                price: 'від 200 грн',
+                photo: 'https://i.imgur.com/oCzbTuE.jpg'
+            },
+            {
+                id: 19,
+                price: 'від 200 грн',
+                photo: 'https://i.imgur.com/VnLhjJu.jpg'
+            },
+            {
+                id: 20,
+                price: 'від 200 грн',
+                photo: 'https://i.imgur.com/VlOPMik.jpg'
             }
         ]
     },
     {
         productId: 'women-dresses',
-        value: []
+        value: [
+            {
+                id: 1,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/tlMwVSJ.jpg'
+            },
+            {
+                id: 2,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/ydVL0GW.jpg'
+            },
+            {
+                id: 3,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/VAGFbHr.jpg'
+            },
+            {
+                id: 4,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/h3uCmhH.jpg'
+            },
+            {
+                id: 5,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/Srrbotj.jpg'
+            },
+            {
+                id: 6,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/cvzWbE7.jpg'
+            },
+            {
+                id: 7,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/Wo44L4R.jpg'
+            },
+            {
+                id: 8,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/Nau6lTp.jpg'
+            },
+            {
+                id: 9,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/boADd3Z.jpg'
+            },
+            {
+                id: 10,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/SG3YrKv.jpg'
+            },
+            {
+                id: 11,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/eM87BZT.jpg'
+            },
+            {
+                id: 12,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/4AMpFyb.jpg'
+            },
+            {
+                id: 13,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/MuwHYS2.jpg'
+            },
+            {
+                id: 14,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/Ov0S08O.jpg'
+            },
+            {
+                id: 15,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/Spb6d0z.jpg'
+            },
+            {
+                id: 16,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/EABRdmu.jpg'
+            },
+            {
+                id: 17,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/1kdRL0R.jpg'
+            },
+            {
+                id: 18,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/JoFJdss.jpg'
+            },
+            {
+                id: 19,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/IKBPh6y.jpg'
+            },
+            {
+                id: 20,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/9an71bj.jpg'
+            },
+            {
+                id: 21,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/qLTWhxi.jpg'
+            },
+            {
+                id: 22,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/F1Ff32e.jpg'
+            },
+            {
+                id: 23,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/IoOnr8C.jpg'
+            },
+            {
+                id: 24,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/s2m01Aw.jpg'
+            },
+            {
+                id: 25,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/xdOOU4Z.jpg'
+            },
+            {
+                id: 26,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/glmzho4.jpg'
+            },
+            {
+                id: 27,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/UmQDu9p.jpg'
+            },
+            {
+                id: 28,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/cVnm5EJ.jpg'
+            },
+            {
+                id: 29,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/TPsYzhc.jpg'
+            },
+            {
+                id: 30,
+                price: 'від 300 грн',
+                photo: 'https://i.imgur.com/GVvCLse.jpg'
+            }
+        ]
     },
     {
         productId: 'paired-suits',
-        value: []
+        value: [
+            {
+                id: 1,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/7fimDBY.jpg'
+            },
+            {
+                id: 2,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/Ncre7Ps.jpg'
+            },
+            {
+                id: 3,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/zh7YTPu.jpg'
+            },
+            {
+                id: 4,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/fG8uOAZ.jpg'
+            }
+        ]
     },
     {
         productId: 'clothes-for-girls-blouses',
-        value: []
+        value: [
+            {
+                id: 1,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/K8CefDS.jpg'
+            },
+            {
+                id: 2,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/VWRD7Ch.jpg'
+            },
+            {
+                id: 3,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/EKIpkxo.jpg'
+            },
+            {
+                id: 4,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/lGb7bOB.jpg'
+            }
+        ]
     },
     {
         productId: 'clothes-for-girls-dresses',
-        value: []
+        value: [
+            {
+                id: 1,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/L6ALsYN.jpg'
+            },
+            {
+                id: 2,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/Ucw2nFo.jpg'
+            },
+            {
+                id: 3,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/pELPq78.jpg'
+            },
+            {
+                id: 4,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/lrpIAZP.jpg'
+            }
+        ]
     },
     {
         productId: 'clothes-for-girls-skirts',
@@ -59,23 +430,184 @@ const data = [
     },
     {
         productId: 'clothes-for-boys-shirt',
-        value: []
+        value: [
+            {
+                id: 1,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/TIzULJ8.jpg'
+            }
+        ]
     },
     {
         productId: 'clothes-for-boys-trousers',
-        value: []
+        value: [
+            {
+                id: 1,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/TIzULJ8.jpg'
+            }
+        ]
     },
     {
         productId: 'stage-clothes',
-        value: []
+        value: [
+            {
+                id: 1,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/yWOeh9l.jpg'
+            },
+            {
+                id: 2,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/AlAKeZ8.jpg'
+            },
+            {
+                id: 3,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/4T9hSX3.jpg'
+            },
+            {
+                id: 4,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/EacrUyM.jpg'
+            },
+            {
+                id: 5,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/PYeoWm4.jpg'
+            },
+
+            {
+                id: 6,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/l0iI4V5.jpg'
+            },
+            {
+                id: 7,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/w7CXXcX.jpg'
+            },
+            {
+                id: 8,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/u32fL9A.jpg'
+            },
+            {
+                id: 9,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/tmBEbP2.jpg'
+            },
+            {
+                id: 10,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/wMD0eVz.jpg'
+            },
+            {
+                id: 11,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/iUUSoTD.jpg'
+            },
+            {
+                id: 12,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/WZSx5AH.jpg'
+            },
+            {
+                id: 13,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/XcBi3HA.jpg'
+            },
+            {
+                id: 14,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/vkuRbic.jpg'
+            },
+            {
+                id: 15,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/02TtF7g.jpg'
+            },
+            {
+                id: 16,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/lBT3z01.jpg'
+            },
+            {
+                id: 17,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/FE2Kk7L.jpg'
+            }
+        ]
     },
     {
         productId: 'tracksuits',
-        value: []
+        value: [            
+            {
+                id: 1,
+                price: 'від 400 грн',
+                photo: 'https://i.imgur.com/cOYGcEe.jpg'
+            },
+            {
+                id: 2,
+                price: 'від 400 грн',
+                photo: 'https://i.imgur.com/cVL02Wi.jpg'
+            },
+            {
+                id: 3,
+                price: 'від 400 грн',
+                photo: 'https://i.imgur.com/AYF4EwT.jpg'
+            },
+            {
+                id: 4,
+                price: 'від 400 грн',
+                photo: 'https://i.imgur.com/A1d7E9f.jpg'
+            },
+            {
+                id: 5,
+                price: 'від 400 грн',
+                photo: 'https://i.imgur.com/oE6ogGT.jpg'
+            },
+            {
+                id: 6,
+                price: 'від 400 грн',
+                photo: 'https://i.imgur.com/20UGBo0.jpg'
+            },
+            {
+                id: 7,
+                price: 'від 400 грн',
+                photo: 'https://i.imgur.com/HPASNWL.jpg'
+            },
+            {
+                id: 8,
+                price: 'від 400 грн',
+                photo: 'https://i.imgur.com/UgiF3r1.jpg'
+            },
+            {
+                id: 9,
+                price: 'від 400 грн',
+                photo: 'https://i.imgur.com/SEdYZoh.jpg'
+            },
+            {
+                id: 10,
+                price: 'від 400 грн',
+                photo: 'https://i.imgur.com/ZU0RZhO.jpg'
+            },
+            {
+                id: 11,
+                price: 'від 400 грн',
+                photo: 'https://i.imgur.com/eN4wLyZ.jpg'
+            }
+        ]
     },
     {
         productId: 'scene-design',
-        value: []
+        value: [
+            {
+                id: 1,
+                price: 'від 500 грн',
+                photo: 'https://i.imgur.com/tI7z3wg.jpg'
+            }
+        ]
     },
     {
         productId: 'trifles',
