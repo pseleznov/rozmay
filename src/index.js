2f6ab89a1b780db68adfab70a3c26adb2cdd4d1a
pseleznov <pashaselesniov1@gmail.com>
2020-08-16 18:05:49 +0300

    remove strict mode

--- src/index.js
+++ src/index.js
@@ -10,13 +10,11 @@ import * as serviceWorker from './serviceWorker';
 const store = getStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
