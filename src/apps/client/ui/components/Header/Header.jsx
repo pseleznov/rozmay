70b8cd6d5cafbe57736579791628bbbddd96f9a4
pseleznov <pashaselesniov1@gmail.com>
2020-08-30 23:13:35 +0300

    connect langMap to header

--- src/apps/client/ui/components/Header/Header.jsx
+++ src/apps/client/ui/components/Header/Header.jsx
@@ -1,35 +1,44 @@
-import React from 'react'
-import './Header.css'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import setLang from '../../../actions/setLang';
+
+import './Header.css';
 
 const Header = () => {
-    return <div className='headerContainer'>
-        <div className='headerContentContainer'>
-            <div className='headerTop_menu'>
-                <NavLink
-                    to={'/'}
-                    exact={true}
-                    className='link'
-                    activeClassName='active'
-                >
-                    Головна
-                </NavLink>
-                <NavLink
-                    to={'/about'}
-                    exact={true}
-                    className='link'
-                    activeClassName='active'
-                >
-                    Про нас
-                </NavLink>
-            </div>
+    const langMap = useSelector(({ application }) => application.langMap);
+    const { main, about } = langMap.header.menu;
+    const dispatch = useDispatch();
 
-            <div className='headerTop_langs'>
-                <div className='lang'>укр</div>
-                <div className='lang'>рус</div>
+    return (
+        <div className='headerContainer'>
+            <div className='headerContentContainer'>
+                <div className='headerTop_menu'>
+                    <NavLink
+                        to={'/'}
+                        exact={true}
+                        className='link'
+                        activeClassName='active'
+                    >
+                        {main}
+                    </NavLink>
+                    <NavLink
+                        to={'/about'}
+                        exact={true}
+                        className='link'
+                        activeClassName='active'
+                    >
+                        {about}
+                    </NavLink>
+                </div>
+
+                <div className='headerTop_langs'>
+                    <div className='lang' onClick={() => dispatch(setLang('ua'))}>укр</div>
+                    <div className='lang' onClick={() => dispatch(setLang('ru'))}>рус</div>
+                </div>
             </div>
         </div>
-    </div>
+    );
 }
 
 export default Header;
