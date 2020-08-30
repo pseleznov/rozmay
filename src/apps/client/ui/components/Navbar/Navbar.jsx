31ca16719c58fdac3f9ce64212234657b247a05b
pseleznov <pashaselesniov1@gmail.com>
2020-08-30 22:17:11 +0300

    navbar refactoring

--- src/apps/client/ui/components/Navbar/Navbar.jsx
+++ src/apps/client/ui/components/Navbar/Navbar.jsx
@@ -1,147 +1,103 @@
-import React, { useState } from 'react';
+import React from 'react';
 import classNames from 'classnames';
-
 import { NavLink } from 'react-router-dom';
 
 import styles from './Navbar.module.css'
+import { Component } from 'react';
 
-const subtitles = {
-    girls: [
-        { title: 'Блузки', link: '/products/clothes-for-girls-blouses' },
-        { title: 'Плаття', link: '/products/clothes-for-girls-dresses' },
-        { title: 'Спіднички', link: '/products/clothes-for-girls-skirts' }
-    ],
-    boys: [
-        { title: 'Сорочки', link: '/products/clothes-for-boys-shirt' },
-        { title: 'Шаровари', link: '/products/clothes-for-boys-trousers' },
-    ]
-};
+const navbar = [
+    { title: 'Чоловічі сорочки', link: '/products/men-shirt' },
+    { title: 'Жіночі блузки', link: '/products/women-blouses' },
+    { title: 'Жіночі сукні', link: '/products/women-dresses' },
+    { title: 'Парні костюми', link: '/products/paired-suits' },
+    {
+        isGroup: true,
+        name: 'Одяг для дівчаток',
+        group: [
+            { title: 'Блузки', link: '/products/clothes-for-girls-blouses' },
+            { title: 'Плаття', link: '/products/clothes-for-girls-dresses' },
+            { title: 'Спіднички', link: '/products/clothes-for-girls-skirts' }
+        ]
+    },
+    {
+        isGroup: true,
+        name: 'Одяг для хлопчиків',
+        group: [
+            { title: 'Сорочки', link: '/products/clothes-for-boys-shirt' },
+            { title: 'Шаровари', link: '/products/clothes-for-boys-trousers' }
+        ]
+    },
+    { title: 'Сценічний одяг', link: '/products/stage-clothes' },
+    { title: 'Спортивні костюми', link: '/products/tracksuits' },
+    { title: 'Оформлення сцен', link: '/products/scene-design' },
+    { title: 'Дрібнички і не тільки', link: '/products/trifles' }
+];
 
-const Navbar = () => {
-    const [showGirlsSubItems, handleGirlsClick] = useState(false);
-    const [showBoysSubItems, handleBoysClick] = useState(false);
+class Navbar extends Component {
 
-    return (
-        <div className={styles.navbarContainer}>
-            <div className={styles.contentContainer}>
-                <div className={styles.title}>КАТАЛОГ</div>
-                <div className={styles.navbarItems}>
-                    <div className={styles.navbarItem}>
-                        <NavLink
-                            to={'/products/men-shirt'}
-                            activeClassName={styles.active}
-                        >
-                            Чоловічі сорочки
-                        </NavLink>
-                    </div>
-                    <div className={styles.navbarItem}>
-                        <NavLink
-                            to={'/products/women-blouses'}
-                            activeClassName={styles.active}
-                        >
-                            Жіночі блузки
-                        </NavLink>
-                    </div>
-                    <div className={styles.navbarItem}>
-                        <NavLink
-                            to={'/products/women-dresses'}
-                            activeClassName={styles.active}
-                        >
-                            Жіночі сукні
-                        </NavLink>
-                    </div>
-                    <div className={styles.navbarItem}>
-                        <NavLink
-                            to={'/products/paired-suits'}
-                            activeClassName={styles.active}
-                        >
-                            Парні костюми
-                        </NavLink>
-                    </div>
-                    <div className={classNames(styles.navbarItem, styles.group)}>
-                        <div className={styles.groupTitle} onClick={() => handleGirlsClick(!showGirlsSubItems)}>
-                            Одяг для дівчаток
-                        </div>
-                        <div className={classNames(styles.itemContent, {
-                            [styles.disable]: !showGirlsSubItems
-                        })}>
-                            {subtitles.girls.map((item, i) => {
-                                return (
-                                    <div
-                                        className={styles.subItem}
-                                        key={i}
-                                    >
-                                        <NavLink
-                                            to={item.link}
-                                            activeClassName={styles.active}
-                                        >
-                                            {item.title}
-                                                </NavLink>
-                                    </div>
-                                );
-                            })}
-                        </div>
-                    </div>
-                    <div className={classNames(styles.navbarItem, styles.group)}>
-                        <div className={styles.groupTitle} onClick={() => handleBoysClick(!showBoysSubItems)}>
-                            Одяг для хлопчиків
-                        </div>
-                        <div className={classNames(styles.itemContent, {
-                            [styles.disable]: !showBoysSubItems
-                        })}>
-                            {subtitles.boys.map((item, i) => {
-                                return (
-                                    <div
-                                        className={styles.subItem}
-                                        key={i}
-                                    >
-                                        <NavLink
-                                            to={item.link}
-                                            activeClassName={styles.active}
-                                        >
-                                            {item.title}
-                                                </NavLink>
-                                    </div>
-                                );
-                            })}
-                        </div>
-                    </div>
-                    <div className={styles.navbarItem}>
-                        <NavLink
-                            to={'/products/stage-clothes'}
-                            activeClassName={styles.active}
-                        >
-                            Сценічний одяг
-                        </NavLink>
-                    </div>
-                    <div className={styles.navbarItem}>
-                        <NavLink
-                            to={'/products/tracksuits'}
-                            activeClassName={styles.active}
-                        >
-                            Спортивні костюми
-                        </NavLink>
-                    </div>
-                    <div className={styles.navbarItem}>
-                        <NavLink
-                            to={'/products/scene-design'}
-                            activeClassName={styles.active}
-                        >
-                            Оформлення сцен
-                        </NavLink>
-                    </div>
-                    <div className={styles.navbarItem}>
-                        <NavLink
-                            to={'/products/trifles'}
-                            activeClassName={styles.active}
-                        >
-                            Дрібнички і не тільки
-                        </NavLink>
+    state = {}
+
+    handleClick = (groupName) => () => {
+        this.setState({
+            [groupName]: !this.state[groupName]
+        });
+    }
+
+    render() {
+
+        return (
+            <div className={styles.navbarContainer}>
+                <div className={styles.contentContainer}>
+                    <div className={styles.title}>КАТАЛОГ</div>
+                    <div className={styles.navbarItems}>
+                        {
+                            navbar.map((item, i) => {
+                                if (item.isGroup) {
+                                    return (
+                                        <div className={classNames(styles.navbarItem, styles.group)}>
+                                            <div className={styles.groupTitle} onClick={this.handleClick(item.name)}>
+                                                {item.name}
+                                            </div>
+                                            <div className={classNames(styles.itemContent, {
+                                                [styles.disable]: this.state[item.name]
+                                            })}>
+                                                {item.group.map((item, i) => {
+                                                    return (
+                                                        <div
+                                                            className={styles.subItem}
+                                                            key={i}
+                                                        >
+                                                            <NavLink
+                                                                to={item.link}
+                                                                activeClassName={styles.active}
+                                                            >
+                                                                {item.title}
+                                                            </NavLink>
+                                                        </div>
+                                                    );
+                                                })}
+                                            </div>
+                                        </div>
+                                    ) 
+                                } else {
+                                    return (
+                                        <div className={styles.navbarItem}>
+                                            <NavLink
+                                                to={`${item.link}`}
+                                                activeClassName={styles.active}
+                                            >
+                                                {item.title}
+                                            </NavLink>
+                                        </div>
+                                    );
+                                }
+                            })
+                        }
                     </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 };
 
 export default Navbar;
