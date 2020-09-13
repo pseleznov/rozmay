1ba61f8fe72258e006d96d9b384846010a35e0c3
pseleznov <pashaselesniov1@gmail.com>
2020-09-13 12:10:51 +0300

    footer refactoring

--- src/apps/client/ui/components/Footer/Footer.jsx
+++ src/apps/client/ui/components/Footer/Footer.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
+import React from 'react';
+import footerBg from './img/background.png';
+
 import './Footer.css'
 
 const Footer = () => {
-    return <div className='footerContainer'>
-        <div className='footerItem'>
-            <div className='footerItem_name'>Наша адреса</div>
-            <div className='footerItem_content'>05440, м.Луцьк вул. Червона 3</div>
+    return (
+        <div className='footerContainer'>
+            <div className='footerBackgroundWrap'>
+                <img src={footerBg} alt="bg"/>
+            </div>
+            <div className='footerContentContainer'>
+                <div className='footerItem'>
+                    <div className='footerItem_name'>Наша адреса:</div>
+                    <div className='footerItem_content'>05440, м.Луцьк вул. Червона 3</div>
+                </div>
+                <div className='footerItem'>
+                    <div className='footerItem_name'>Телефон:</div>
+                    <div className='footerItem_content'>098-765-43-21</div>
+                </div>
+                <div className='footerItem'>
+                    <div className='footerItem_name'>Електронна адреса:</div>
+                    <div className='footerItem_content'>Rozmay@gmail.com</div>
+                </div>
+            </div>
         </div>
-        <div className='footerItem'>
-            <div className='footerItem_name'>Телефон</div>
-            <div className='footerItem_content'>098-765-43-21</div>
-        </div>
-        <div className='footerItem'>
-            <div className='footerItem_name'>Електронна адреса</div>
-            <div className='footerItem_content'>Rozmay@gmail.com</div>
-        </div>
-    </div>
+    );
 }
 
 export default Footer;
